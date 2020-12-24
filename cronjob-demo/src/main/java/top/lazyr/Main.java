76a3d6a990135743c2427627d5cfb44d02b3b5e7
lazy-r <2296339656@qq.com>
2020-12-24 15:32:19 +0800

    =cronjob-demo finished

--- cronjob-demo/src/main/java/top/lazyr/Main.java
+++ cronjob-demo/src/main/java/top/lazyr/Main.java
@@ -9,38 +9,7 @@ public class Main {
     public static void main(String[] args) {
         SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日hh时mm分ss秒");
         String currentTime = format.format(System.currentTimeMillis());
-        writeFile("/home/k8s/logs/"+currentTime+".log",currentTime+"启动了项目");
+        System.out.println(currentTime+"执行了该程序");
     }
 
-    /**
-     * 将content写入到指定path的文件中
-     * @param path
-     * @param content
-     */
-    public static void writeFile(String path,String content){
-        File file = new File(path);
-        System.out.println(file.getAbsolutePath());
-        FileWriter writer = null;
-        boolean succeed = true;
-        try {
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            writer = new FileWriter(file,true);
-            writer.write(content);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            succeed = false;
-        }finally {
-            if(writer != null){
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("文件"+file.getName()+"写入"+(succeed?"成功":"失败"));
-        }
-    }
 }
