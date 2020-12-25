9c425f78392a7ba2c4e5fc22dd2b49c3a03d0c16
lazy-r <2296339656@qq.com>
2020-12-25 20:05:42 +0800

    =normal

--- springboot-demo/src/main/java/top/lazyr/controller/HelloController.java
+++ springboot-demo/src/main/java/top/lazyr/controller/HelloController.java
@@ -1,6 +1,5 @@
 package top.lazyr.controller;
 
-import com.sun.org.glassfish.gmbal.ParameterNames;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
