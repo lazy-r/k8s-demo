328c99b6df48b4571ec94c86f37128f8e54cc646
lazy-r <2296339656@qq.com>
2020-12-25 20:01:39 +0800

    =update complete springboot-demo

--- springboot-demo/src/main/java/top/lazyr/controller/HelloController.java
+++ springboot-demo/src/main/java/top/lazyr/controller/HelloController.java
@@ -1,4 +1,15 @@
 package top.lazyr.controller;
 
+import com.sun.org.glassfish.gmbal.ParameterNames;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name){
+        return "<h1 align='center'><span style='color: red'>"+name+"</span>,你好</h1>";
+    }
 }
